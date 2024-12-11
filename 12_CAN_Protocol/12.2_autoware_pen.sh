c4ca8094a642f6b28342b0485fd69a08c95006fa
nahyunHycon <nahyun.kim@hyconsoft.com>
2024-12-11 15:04:29 +0900

    modified ttcn3 script

--- 12_CAN_Protocol/12.2_autoware_pen.sh
+++ 12_CAN_Protocol/12.2_autoware_pen.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 LOG_FILE="/home/hyconsoft_rnd/Desktop/TestScript/09_LogVerification/log/systemlog.log"
-echo "$(date '+%Y-%m-%d %H:%M:%S') - [12.2] CAN_Pen Test started" >> "$LOG_FILE"
-
-
-
+echo "$(date '+%Y-%m-%d %H:%M:%S') - [12.1] CAN_Pen Test started" >> "$LOG_FILE"
 
 
 dir="/home/hyconsoft_rnd/Desktop/TestScript/12_CAN_Protocol/result/"
@@ -13,14 +10,14 @@ file_count=$(find "$dir" -type f | wc -l)
 extracted_value="4"
 
 if [ "$extracted_value" == "$file_count" ]; then
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - [12.2] CAN_Pen Test result = 0" >> "$LOG_FILE" 
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - [12.1] CAN_Pen Test result = 0" >> "$LOG_FILE" 
   echo 0
 else
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - [12.2] CAN_Pen Test result = 1" >> "$LOG_FILE" 
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - [12.1] CAN_Pen Test result = 1" >> "$LOG_FILE" 
   echo 1
 fi
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') - [12.2] CAN_Pen Test finished" >> "$LOG_FILE"
+echo "$(date '+%Y-%m-%d %H:%M:%S') - [12.1] CAN_Pen Test finished" >> "$LOG_FILE"
 
 
 TARGET_DIR="/home/hyconsoft_rnd/Desktop/TestScript/12_CAN_Protocol/result/"
